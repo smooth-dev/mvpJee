06b610e7eee4be4d9897bc89bbfacecfe2f10674
Azh Yasine <azhar.yassine2015@gmail.com>
2020-12-16 12:35:41 +0100

    Rest application without AOP , jsp files , war packaging

---
+++ src/test/java/com/howtodoinjava/demo/DemoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.howtodoinjava.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
